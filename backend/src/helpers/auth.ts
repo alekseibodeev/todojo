e109b1ee23e01d446e2ad28209dbe3dcbf2fdb49
Aleksei Bodeev <bodeevaleksei@gmail.com>
2025-04-09 14:09:11 +0500

    :lock: feat(backend): Add core auth utilities

---
+++ backend/src/helpers/auth.ts
@@ -0,0 +1,22 @@
+import { config } from '../config/config.ts';
+import bcrypt from 'bcrypt';
+import type { Request } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (userId: string) => {
+  return jwt.sign(userId, config.tokenSecret, {
+    expiresIn: config.tokenExpires,
+  });
+};
+
+export const hashPassword = async (password: string) => {
+  return bcrypt.hash(password, 10);
+};
+
+export const comparePassword = async (password: string, hash: string) => {
+  return bcrypt.compare(password, hash);
+};
+
+export const getAuthToken = (req: Request) => {
+  return req.headers.authorization?.trim().replace(/^Bearer\s+/i, '');
+};
