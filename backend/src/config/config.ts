e38062bdc577cd47f78ae380dbaa177f17a1d0e9
Aleksei Bodeev <bodeevaleksei@gmail.com>
2025-04-09 14:08:31 +0500

    :gear: feat(backend): Add typed env config

---
+++ backend/src/config/config.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+
+export const config: {
+  port: number;
+  tokenSecret: string;
+  tokenExpires: number;
+} = {
+  port: Number(process.env.PORT),
+  tokenSecret: process.env.JWT_SECRET_KEY!,
+  tokenExpires: Number(process.env.JWT_EXPIRES_IN),
+};
