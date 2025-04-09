d30912f8bbbd01950c1403ec0420a9d5f00f5480
Aleksei Bodeev <bodeevaleksei@gmail.com>
2025-04-09 15:35:30 +0500

    :construction: feat(backend): Add auth endpoint stubs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,11 @@
+import authRouter from './routes/auth.ts';
 import express from 'express';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/auth', authRouter);
 
 app.get('/', (_req, res) => {
   res.send('<h1>Hello, World</h1>');
---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const authRouter = express.Router();
+
+authRouter.post('/register', () => {
+  // TODO
+});
+
+authRouter.post('/login', () => {
+  // TODO
+});
+
+authRouter.get('/me', () => {
+  // TODO
+});
+
+export default authRouter;
