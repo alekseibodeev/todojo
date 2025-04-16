83d98447078b6f90adaf89167a2ed267b16dc880
Aleksei Bodeev <bodeevaleksei@gmail.com>
2025-04-16 12:03:43 +0500

    :sparkles: feat(auth): Add auth form components

---
+++ frontend/src/components/LoginForm.tsx
@@ -0,0 +1,25 @@
+import Button from './Button';
+import FormField from './FormField';
+import Input from './Input';
+import { FormHTMLAttributes } from 'react';
+import Link from './Link';
+
+const LoginForm = ({
+  ...props
+}: Omit<FormHTMLAttributes<HTMLFormElement>, 'children'>) => (
+  <form className="grid rounded-xl bg-white p-8" {...props}>
+    <h1 className="mb-4 text-center text-2xl font-bold">Login</h1>
+    <FormField label="Email">
+      <Input id="email" type="email" required />
+    </FormField>
+    <FormField label="Password">
+      <Input id="password" type="password" required />
+    </FormField>
+    <Button>Submit</Button>
+    <p className="mt-8 text-center text-sm">
+      Don't have an account yet? <Link to="/register">Register</Link>
+    </p>
+  </form>
+);
+
+export default LoginForm;
---
+++ frontend/src/components/RegisterForm.tsx
@@ -0,0 +1,25 @@
+import Button from './Button';
+import FormField from './FormField';
+import Input from './Input';
+import { FormHTMLAttributes } from 'react';
+import Link from './Link';
+
+const RegisterForm = ({
+  ...props
+}: Omit<FormHTMLAttributes<HTMLFormElement>, 'children'>) => (
+  <form className="grid rounded-xl bg-white p-8" {...props}>
+    <h1 className="mb-4 text-center text-2xl font-bold">Register</h1>
+    <FormField label="Email">
+      <Input id="email" type="email" required />
+    </FormField>
+    <FormField label="Password">
+      <Input id="password" type="password" required />
+    </FormField>
+    <Button>Submit</Button>
+    <p className="mt-8 text-center text-sm">
+      Already have an account? <Link to="/login">Login</Link>
+    </p>
+  </form>
+);
+
+export default RegisterForm;
