e16de3833c194fc38488ea59c747f23fe2f43c5e
Aleksei Bodeev <bodeevaleksei@gmail.com>
2025-04-17 16:00:57 +0500

    :sparkles: feat(ui): Add popover with project actions

--- frontend/src/components/ProjectItem.tsx
+++ frontend/src/components/ProjectItem.tsx
@@ -1,7 +1,19 @@
+import { Popover, PopoverContent, PopoverTrigger } from '../components/Popover';
 import { Project } from '../types/api';
+import Button from './Button';
+import { Ellipsis } from 'lucide-react';
+import { MouseEventHandler } from 'react';
 import { NavLink } from 'react-router';
 
-const ProjectItem = ({ project }: { project: Project }) => (
+const ProjectItem = ({
+  project,
+  onEdit,
+  onDelete,
+}: {
+  project: Project;
+  onEdit: MouseEventHandler<HTMLButtonElement>;
+  onDelete: MouseEventHandler<HTMLButtonElement>;
+}) => (
   <li className="flex">
     <NavLink
       className={({ isActive }) =>
@@ -12,6 +24,19 @@ const ProjectItem = ({ project }: { project: Project }) => (
     >
       {project.title}
     </NavLink>
+    <Popover>
+      <PopoverTrigger asChild>
+        <button>
+          <Ellipsis />
+        </button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="grid w-48 gap-2 p-4">
+          <Button onClick={onEdit}>Edit</Button>
+          <Button onClick={onDelete}>Delete</Button>
+        </div>
+      </PopoverContent>
+    </Popover>
   </li>
 );
 
